d493a64fe6b59d5ee9e625142cadb73f68370745
delvering17 <delvering17@gmail.com>
2023-10-31 00:01:26 +0900

    4-5) 인증 및 인가 예외 처리 - AjaxLoginUrlAuthenticationEntryPoint, AjaxAccessDeniedHandler

--- src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
@@ -3,6 +3,7 @@ package io.security.corespringsecurity.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MessageController {
@@ -12,4 +13,11 @@ public class MessageController {
 
 		return "user/messages";
 	}
+
+	@ResponseBody
+	@GetMapping("/api/messages")
+	public String apiMessage() {
+		return "messages ok";
+	}
+
 }
